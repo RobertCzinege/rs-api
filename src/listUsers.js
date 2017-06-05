0c61d1c366d3163ac85f16eefe38b911d0b09780
Robert Czinege <robert.czinege@blackswan.com>
2017-06-05 03:41:54 +0200

    Json web token verification added.

--- src/listUsers.js
+++ src/listUsers.js
@@ -1,12 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { config } from './config.js';
+
 export const listUsers = (req, res, db) => {
 
-  db.collection('Users').find({}).toArray()
-  .then(([result]) => {
-    result.length == 0 ? res.send('There are no users registered.') : res.status(200).json(result);
-  })
-  .catch(([err]) => {
-    console.log(err);
-    res.status(500).json({msg: 'Something went wrong'});
+  let token = req.query.token;
+  jwt.verify(token, config.secret, (err, decode) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({success: false, msg: 'Invalid token'});
+
+    } else {
+      db.collection('Users').find({}).toArray()
+      .then((result) => {
+        result.length == 0 ? res.status(200).send({success: true, msg: 'There are no users registered.' }) : res.status(200).json({success: true, users: result});
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({success: false, msg: 'Something went wrong'});
+      });
+    }
   });
 
 }
--- src/registerUser.js
+++ src/registerUser.js
@@ -1,12 +1,13 @@
 import { generateRandomString } from './helper/generateRandomString.js';
 import { sendMail } from './helper/mailSetup.js';
 import { config } from './config.js';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = (req, res, db) => {
 
   let newUser = {
     name: req.body.name,
-    email: req.body.email
+    email: req.body.email,
   };
 
   db.collection('Users').find({email: newUser.email}).toArray()
@@ -14,15 +15,20 @@ export const registerUser = (req, res, db) => {
     if (result.length == 0) {
       db.collection('Users').insert(newUser)
       .then(() => {
-        sendMail(newUser, `${config.serverAddress}/users`);
-        res.status(200).json({msg: `User ${req.body.name} has been added to the database. Thank you for registering`});
+
+        const token = jwt.sign(newUser, config.secret, {
+          expiresIn: '1h'
+        });
+
+        sendMail(newUser, `${config.serverAddress}:${config.port}/users?token=${token}`);
+        res.status(200).json({success: true, msg: `User ${req.body.name} has been added to the database. An e-mail has been sent to the given address with a link to list the users.`});
       })
       .catch((err) => {
         console.log(err);
-        res.status(500).json({msg: 'Something went wrong'});
+        res.status(500).json({success: false, msg: 'Something went wrong'});
       })
     } else {
-      res.status(400).json({msg: 'E-mail address is already registered.'});
+      res.status(400).json({success: false, msg: 'E-mail address is already registered.'});
     }
   })
 
